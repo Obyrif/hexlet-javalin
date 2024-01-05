9d0cbd469f9604b3dcc1c45d4edf4b46db763f66
Obydennov Dmitrii <dima-obydennov@mail.ru>
2024-01-05 15:59:22 +0200

    new lessons

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,13 +1,25 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.config.JavalinConfig;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
-        var app = Javalin.create(config -> config.plugins.enableDevLogging());
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        var app = getJavalin();
+        app.start(7070);
+    }
+
+    public static Javalin getJavalin() {
+        var app = Javalin.create((JavalinConfig config) -> config.plugins.enableDevLogging());
+
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
+
+        return app;
     }
 }
